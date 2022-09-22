5e821dc935c178189d646d5cf077eab95f8739fb
LHenriqueDeAndrade <henriquesilvahenrique-2014@outlook.com>
2022-09-22 18:31:35 -0300

    atualizaçao footer

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,10 +1,42 @@
 import React from "react";
 import * as S from "../Styles/FooterStyle.js";
+import instagram from "../assets/Instagram Ellipse.png";
+import twitter from "../assets/Twitter ellipse.png";
+import facebook from "../assets/Facebook Ellipse.png";
+import pinterest from "../assets/Pinterest Ellipse.png";
 
 export default function Footer() {
   return (
     <S.footerDiv>
-      <div></div>
+      <S.Subscription>
+        <div>
+          <h2>SUBSCRIBE</h2>
+          <p>Sign up for newsletter</p>
+          <input placeholder="Your Email"></input>
+          <button>SUBMIT</button>
+        </div>
+      </S.Subscription>
+
+      <S.quickAcess>
+        <S.socialMedia>
+          <img src={instagram} alt="instagram" />
+          <img src={twitter} alt="twitter" />
+          <img src={facebook} alt="facebook" />
+          <img src={pinterest} alt="pinterest" />
+        </S.socialMedia>
+        <S.footerMenu>
+          <li>ABOUT</li>
+          <li>RECIPES</li>
+          <li>SUBSCRIBE</li>
+        </S.footerMenu>
+      </S.quickAcess>
+      <S.Rights>
+        <p>
+          Layout produzido por Vai na Web para fins exclusivamente educacionais.
+          Referência:
+          https://br.pinterest.com/pin/AVuDlMAl4GsQiM6nijH9YbG9bsNKpompSEOEHzig6GJ58AnUtMkSy7k/
+        </p>
+      </S.Rights>
     </S.footerDiv>
   );
 }
--- Styles/FooterStyle.js
+++ Styles/FooterStyle.js
@@ -1,5 +1,151 @@
 import styled from "styled-components";
 
 export const footerDiv = styled.div`
-  border: none;
+  width: 100%;
+`;
+
+export const Subscription = styled.section`
+  background-color: #dfe4de;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  div {
+    position: relative;
+    h2 {
+      display: flex;
+      margin-bottom: 2vh;
+      justify-content: center;
+    }
+    p {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 3vh;
+    }
+    input {
+      background-color: #eff1ee;
+      height: 10vh;
+      width: 40vw;
+      text-shadow: none;
+      text-align: center;
+      &::placeholder {
+        position: absolute;
+        top: 3vh;
+        left: 2vw;
+        opacity: 0.5;
+        margin-left: 5px;
+      }
+    }
+    button {
+      background-color: transparent;
+      border: solid;
+      font-weight: bold;
+      width: 10vw;
+      height: 8vh;
+      font-size: 1.2em;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-left: 15vw;
+      margin-top: 2vh;
+      &:hover {
+        background-color: #e9ec8a;
+        cursor: pointer;
+      }
+    }
+  }
+  @media only screen and (max-width: 600px) {
+    height: 55vh;
+    div {
+      width: 100vw;
+      height: 40vh;
+      p {
+        font-size: 1.8em;
+      }
+      input {
+        width: 95vw;
+        height: 9vh;
+        margin-left: 2vw;
+      }
+      button {
+        width: 95vw;
+        height: 9vh;
+        margin-left: 2vw;
+      }
+    }
+  }
+`;
+
+export const quickAcess = styled.section`
+  height: 40vh;
+  background-color: #f2f4f1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media only screen and (max-width: 600px) {
+    height: 50vh;
+  }
+`;
+
+export const socialMedia = styled.div`
+  margin-left: 2vw;
+  img {
+    margin-left: 1vw;
+    cursor: pointer;
+  }
+  @media only screen and (max-width: 600px) {
+    position: relative;
+    top: 10vh;
+    margin-left: 15vw;
+    display: flex;
+    img {
+      margin-left: 2vw;
+    }
+  }
+`;
+
+export const footerMenu = styled.ul`
+  display: flex;
+  margin-right: 4vw;
+  & :hover {
+    background-color: #e9ec8a;
+    border: solid;
+    cursor: pointer;
+  }
+  li {
+    font-size: 1.2em;
+    margin-left: 4vw;
+    cursor: pointer;
+  }
+  @media only screen and (max-width: 600px) {
+    display: inline-block;
+    text-align: center;
+    line-height: 5vh;
+    position: relative;
+    bottom: 10vh;
+    right: 52vw;
+  }
+`;
+
+export const Rights = styled.div`
+  background-color: #446061;
+  height: 8vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    color: white;
+    opacity: 0.5;
+    font-size: 0.9em;
+  }
+  @media only screen and (max-width: 600px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: solid;
+    p {
+      margin-left: 4vw;
+      font-size: 0.4em;
+    }
+  }
 `;
