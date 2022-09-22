987eeb093cad547d7496ea1b6ba3b9a42065f8b2
LHenriqueDeAndrade <henriquesilvahenrique-2014@outlook.com>
2022-09-22 20:34:33 -0300

    menu hamburguer adicionado

--- Components/Header.js
+++ Components/Header.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "../Styles/HeaderStyle.js";
 import RC from "../assets/RC.png";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const Modal = () => {
+    return (
+      <S.miniMenu>
+        <img src={RC} alt="rc" />
+        <ul>
+          <li>ABOUT</li>
+          <li>RECIPES</li>
+          <li>SUBSCRIBE</li>
+        </ul>
+      </S.miniMenu>
+    );
+  };
+
   return (
     <S.headerSection>
+      <S.Modal
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {open === true ? "↻" : "≡"}
+      </S.Modal>
+      {open && Modal()}
       <S.Menu>
         <img src={RC} alt="rc" />
         <ul>
--- Styles/HeaderStyle.js
+++ Styles/HeaderStyle.js
@@ -16,6 +16,36 @@ export const GlobalStyle = createGlobalStyle`
 }
 `;
 
+export const miniMenu = styled.nav`
+  position: relative;
+  top: 6vw;
+  img {
+    position: absolute;
+    left: 2vw;
+    width: 6vw;
+  }
+  ul {
+    display: flex;
+    position: absolute;
+    left: 49.4vw;
+    & :hover {
+      background-color: #e9ec8a;
+      border: solid;
+      cursor: pointer;
+    }
+  }
+  li {
+    font-size: 1.8em;
+    margin-left: 6vw;
+  }
+  @media only screen and (max-width: 600px) {
+    font-size: 0.3em;
+    ul {
+      left: 24vw;
+    }
+  }
+`;
+
 export const headerSection = styled.section`
   width: 100%;
   height: 69vw;
@@ -44,6 +74,20 @@ export const headerSection = styled.section`
   }
 `;
 
+export const Modal = styled.button`
+  display: none;
+  font-size: 0.1em;
+  @media only screen and (max-width: 600px) {
+    display: flex;
+    background-color: transparent;
+    color: blue;
+    border: none;
+    font-size: 1.4em;
+    position: relative;
+    left: 85vw;
+  }
+`;
+
 export const Menu = styled.nav`
   position: relative;
   top: 6vw;
@@ -68,6 +112,7 @@ export const Menu = styled.nav`
   }
   @media only screen and (max-width: 600px) {
     font-size: 0.3em;
+    display: none;
     ul {
       left: 24vw;
     }
